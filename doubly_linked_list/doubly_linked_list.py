fb4ea5d7b4fd9b955835fb85b32119c22730a9ee
EricFerguson76 <turtle0888@gmail.com>
2020-05-24 16:06:41 -0500

    Fixed errors

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -112,30 +112,35 @@ class DoublyLinkedList:
     Returns the value of the removed Node."""
 
     def remove_from_tail(self):
-
         value = self.tail.value
-        self.delete(self.tail)
+
+        if not self.tail:
+            return
+        elif self.head == self.tail:
+            self.head = None
+            self.tail = None
+
+        else:
+            self.tail = self.tail.prev
+            self.tail.next = None
+
+        self.length -= 1
 
         return value
     """Removes the input node from its current spot in the
     List and inserts it as the new head node of the List."""
 
     def move_to_front(self, node):
-        if node is self.tail:
-            return
-        self.add_to_head(node.value)
+        value = node.value
         self.delete(node)
-
+        self.add_to_head(value)
     """Removes the input node from its current spot in the
     List and inserts it as the new tail node of the List."""
 
     def move_to_end(self, node):
-
-        if node is self.tail:
-            return
-        self.add_to_tail(node.value)
+        value = node.value
         self.delete(node)
-        self.length - 1
+        self.add_to_tail(value)
     """Removes a node from the list and handles cases where
     the node was the head or the tail"""
 
@@ -164,11 +169,12 @@ class DoublyLinkedList:
     """Returns the highest value currently in the list"""
 
     def get_max(self):
-        current = self.head
-        max = self.head.data
+        highest_value = self.head.value
+        current_node = self.head
 
-        while (current is not None):
-            if current.value > max:
-                max = current.value
-            current = current.next
-        return max
+        while current_node is not None:
+
+            if current_node.value > highest_value:
+                highest_value = current_node.value
+            current_node = current_node.next
+        return highest_value
